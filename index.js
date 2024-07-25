71e75fc4e2602c17f1cc5d517ad11030b34e2c79
NadaJrad <nadajradbenaicha@gmail.com>
2024-07-25 12:07:28 +0200

    using nodemon for http methode

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const port = 3000;
 app.get ("/", (req,res) =>{
 res.send("Hello World!");
 });
+app.get ("/about", (req,res) =>{
+    res.send("<h1>Hello</h1><p>My name is Nada</p>");
+    });
+app.get ("/contact", (req,res) =>{
+        res.send("<h1>contact me </h1><p>email: nadajradbenaicha@gmail.com</p>");
+        });
 app.listen(port,() => {
     console.log(`the server runing in port ${port}`);
 });
